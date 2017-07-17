cb14ce3645d43c9f41c6800a0d034457b71f9acd
Sahil Batla <sahil.batla@wingify.com>
2017-07-18 04:42:12 +0530

    Add way to connect to heroku pg

--- config/sequelize.js
+++ config/sequelize.js
@@ -2,16 +2,22 @@ var config = require ('./config.json');
 var auth = config[config.environment];
 
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize(auth.database, auth.username, auth.password, {
-  host: auth.host,
-  port: auth.port,
-  dialect: 'postgres',
 
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000
-  },
-});
+if (proces.env.DATABASE_URL) {
+	const sequelize = new Sequelize(process.env.DATABASE_URL);
+} else {
+	const sequelize = new Sequelize(auth.database, auth.username, auth.password, {
+	  host: auth.host,
+	  port: auth.port,
+	  dialect: 'postgres',
+
+	  pool: {
+	    max: 5,
+	    min: 0,
+	    idle: 10000
+	  },
+	});
+}
+
 
 module.exports = sequelize;
