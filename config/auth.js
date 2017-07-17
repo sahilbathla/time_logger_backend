baabc25295793e5e7f3cd4b16ff5cfb0e7a239bb
Sahil Batla <sahil.batla@wingify.com>
2017-07-18 04:07:27 +0530

    Add heroku auth

--- config/auth.js
+++ config/auth.js
@@ -2,6 +2,6 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      : '1398289653593676', // your App ID
         'clientSecret'  : 'e7dd30c90ab42ac38b840d2750fec700', // your App Secret
-        'callbackURL'   : 'http://localhost:8082/auth/facebook/callback'
+        'callbackURL'   : 'https://timelogit.herokuapp.com/auth/facebook/callback'
     }
 }
