3a372a930c7faa6df8a4d964ec9e7189090de5c2
Sahil Batla <sahil.batla@wingify.com>
2017-07-18 04:48:58 +0530

    Fix typo

--- config/sequelize.js
+++ config/sequelize.js
@@ -2,11 +2,12 @@ var config = require ('./config.json');
 var auth = config[config.environment];
 
 const Sequelize = require('sequelize');
+var sequelize;
 
 if (process.env.DATABASE_URL) {
-	const sequelize = new Sequelize(process.env.DATABASE_URL);
+	sequelize = new Sequelize(process.env.DATABASE_URL);
 } else {
-	const sequelize = new Sequelize(auth.database, auth.username, auth.password, {
+	sequelize = new Sequelize(auth.database, auth.username, auth.password, {
 	  host: auth.host,
 	  port: auth.port,
 	  dialect: 'postgres',
