c390c5405891fc8332fbec4d3e59c45e35a0164c
Sahil Batla <sahil.batla@wingify.com>
2017-07-18 23:23:49 +0530

    Add readme and remove auth

--- config/auth.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    'facebookAuth' : {
-        'clientID'      : '1398289653593676', // your App ID
-        'clientSecret'  : 'e7dd30c90ab42ac38b840d2750fec700', // your App Secret
-        'callbackURL'   : 'https://timelogit.herokuapp.com/auth/facebook/callback'
-    }
-}
